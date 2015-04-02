e549b30d278d1bff12af12bb25e741ec646d905c
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-02 09:38:39 +0200

    main.py : main compilable

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #Fichier main qui réalise la détection de visages. (En vours de développement)
+
+if __name__ == "__main__" :
+    print("Programme de détection de visages en cours de lancement...")
+
+
+
+
+    print("Fermeture du programme.")
